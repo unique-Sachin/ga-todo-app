60637383fd29b469bc180855cc4f7f3bad043d20
Sachin Mishra <msachin786.pm@gmail.com>
2023-01-07 05:34:28 +0530

    added vercel deloyment process

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -28,7 +28,7 @@ const TodoApp = () => {
   }, []);
   return (
     <div>
-      <h3>TodoApp</h3>
+      <h3>TodoApp Deployment on Vercel</h3>
       <TodoInput
         value={value}
         handleChange={handleChange}
